d37f4592236b6c8d59899a2d5bfbfa2ffa02ee95
Raikish <albert.montagut.casero@gmail.com>
2019-03-13 18:52:10 +0100

    Initial commit with some notes and first banner

---
+++ app/src/main/java/com/montagut/casero/albert/test/MainActivity.java
@@ -0,0 +1,74 @@
+package com.montagut.casero.albert.test;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declarar variable contenedora de un contenedor de anuncios
+    private AdView adViewTop;
+    private AdView adViewBottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Inicializar modulo de ads
+        MobileAds.initialize(this, "ca-app-pub-1661379239296772~8355807797");
+
+        // Crear un banner de anuncios
+        adViewTop = new AdView(this);
+        adViewTop.setAdSize(AdSize.BANNER);
+        adViewTop.setAdUnitId("ca-app-pub-3940256099942544~3347511713");
+
+        adViewBottom = findViewById(R.id.bottom_banner);
+        AdRequest adrequest = new AdRequest.Builder().build();
+        adViewBottom.loadAd(adrequest);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
